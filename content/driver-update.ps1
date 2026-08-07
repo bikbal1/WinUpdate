25605d7c4b286ed2ed8b85794657a993855396f0
B <kryttdesign@gmail.com>
2026-08-07 12:00:32 -0400

    Update driver-update.ps1

--- content/driver-update.ps1
+++ content/driver-update.ps1
@@ -1,32 +1,50 @@
-# Description: This script will install the PSWindowsUpdate module and check for driver updates.
-# It will run in a separate window and check for updates. If there are no updates, it will exit.
-# You can just run this script by itself by running the command:
-# irm https://github.com/SapphSky/PowerExpressGUI/raw/main/content/driver-update.ps1 | iex
+# Description: Installs PSWindowsUpdate, installs available driver updates,
+# and restarts the computer when finished.
 
 # How many attempts we want to try and install the module.
 $MaxAttempts = 5
 
+# Install and import PSWindowsUpdate module
 for ($i = 1; $i -le $MaxAttempts; $i++) {
-    if (-Not (Get-Module -Name PSWindowsUpdate)) {
-        Write-Progress -Activity "Preparing PSWindowsUpdate Module" -Status "Attempt $i of $MaxAttempts"
-        Write-Host 'Getting Package Provider'
-        Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
 
-        Write-Host 'Setting Repository'
+    if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
+
+        Write-Progress -Activity "Preparing PSWindowsUpdate Module" -Status "Attempt $i of $MaxAttempts"
+
+        Write-Host "Thanks to B for making it easier!"
+
+        Write-Host "Getting Package Provider..."
+        Install-PackageProvider -Name NuGet -Force -Confirm:$false | Out-Null
+
+        Write-Host "Setting Repository..."
         Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-        Write-Host 'Installing PSWindowsUpdate Module...'
-        Install-Module -Name PSWindowsUpdate -Force
-        Start-Sleep -Seconds 1
+        Write-Host "Installing PSWindowsUpdate Module..."
+        Install-Module -Name PSWindowsUpdate -Force -Confirm:$false
 
-        Write-Host 'Importing module...'
-        Import-Module -Name PSWindowsUpdate -Force
+        Start-Sleep -Seconds 2
+
+        Write-Host "Importing module..."
+        Import-Module PSWindowsUpdate -Force
+
         Write-Progress -Activity "Preparing PSWindowsUpdate Module" -Completed
     }
     else {
-        Write-Host "Checking for updates..."
-        Install-WindowsUpdate -AcceptAll -UpdateType Driver
+        Import-Module PSWindowsUpdate -Force
         break
     }
-    $i++
 }
+
+# Install driver updates
+# This is the different part from original repository from SapphSky's, instead of prompting yes or no, it forces to restart.
+Write-Host "Checking for driver updates..."
+
+Install-WindowsUpdate -AcceptAll -UpdateType Driver -Confirm:$false
+
+Write-Host "Driver update process completed."
+Write-Host "Restarting computer..."
+
+Start-Sleep -Seconds 5
+
+# Force reboot
+shutdown.exe /r /t 0 /f
